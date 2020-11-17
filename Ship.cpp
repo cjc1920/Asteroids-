02715d352476f0079da593d2858618e572ae9750
Christian <cjcr1820@gmail.com>
2020-11-17 18:07:03 -0400

    Added code to move the ship smoothly

---
+++ Ship.cpp
@@ -0,0 +1,238 @@
+#include "Ship.hpp"
+#include "App.hpp"
+
+#include <gl\GL.h>
+
+//STL
+#include <iostream>
+#include <cmath>
+
+namespace Engine
+{
+	// TODO: RR: Move this to a lib
+	const float PI = 3.141592653;
+	const float MAX_VELOCITY = 500.0f;
+	const float THRUST = 15.0f;
+	const float DRAG_FORCE = 0.999f;
+	const float ANGLE_OFFSET = 90.0f;
+
+	// TODO: RR: Get this out of here!
+
+	inline float wrap(float x, float min, float max)
+	{
+		if (x < min)
+			return max - (min - x);
+		if (x > max)
+			return min + (x - max);
+		return x;
+	}
+
+	Ship::Ship(App *parent)
+		: m_position(Math::Vector2::Origin), m_velocity(Math::Vector2::Origin), m_angle(0.0f), m_rotation(250.0f), m_mass(1.0f), m_parent(parent) //TODO: RR: contemplate using a component based design approach
+
+	{
+		std::cout << "Construction of ship\n";
+		ChangeShip();
+	}
+
+	Ship::Ship(App *parent, float _x, float _y)
+		: m_position(_x, _y), m_velocity(Math::Vector2::Origin), m_angle(0.0f), m_rotation(250.0f), m_mass(1.0f), m_parent(parent)
+
+	{
+		std::cout << "Construction of ship\n";
+	}
+
+	Ship::~Ship()
+	{
+		std::cout << "Destruction of ship\n";
+	}
+
+	void Ship::MoveUp()
+	{
+		ApplyImpulse(Math::Vector2(THRUST));
+	}
+
+	void Ship::RotateLeft(float deltaTime)
+	{
+
+		m_angle += m_rotation * deltaTime;
+	}
+
+	void Ship::RotateRight(float deltaTime)
+	{
+		m_angle -= m_rotation * deltaTime;
+	}
+
+	void Ship::ApplyDrag(Math::Vector2 force)
+	{
+		m_velocity.x *= force.x;
+		m_velocity.y *= force.y;
+	}
+	void Ship::ApplyImpulse(Math::Vector2 impulse)
+	{
+		if (m_mass > 0)
+		{
+			m_velocity.x += (impulse.x / m_mass) * cosf((m_angle + ANGLE_OFFSET) * (PI / 180));
+			m_velocity.y += (impulse.x / m_mass) * sinf((m_angle + ANGLE_OFFSET) * (PI / 180));
+		}
+	}
+
+	void Ship::Update(float deltaTime)
+	{
+		//calculate speed
+		float speed =
+			std::fabs(m_velocity.Length());
+
+		//Cap speed
+		if (speed > MAX_VELOCITY)
+		{
+			m_velocity.x = (m_velocity.x / speed) * MAX_VELOCITY;
+			m_velocity.y = (m_velocity.y / speed) * MAX_VELOCITY;
+		}
+
+		//Set new state
+		m_currentSpeed = speed;
+		m_position.x += m_velocity.x * deltaTime;
+		m_position.y += m_velocity.y * deltaTime;
+
+		//Applies drag
+
+		ApplyDrag(Math::Vector2(DRAG_FORCE));
+
+		//calcuations for wrap around
+		//TODO:: RR:: Create parent class to handle this for all rendered entities
+
+		float halfWidth = m_parent->GetWidth() / 2.0f;
+		float halfHeight = m_parent->GetHeight() / 2.0f;
+
+		float worldMinX = -halfWidth;
+		float worldMaxX = halfWidth;
+
+		float worldMinY = -halfHeight;
+		float worldMaxY = halfHeight;
+
+		m_position.x = wrap(m_position.x, worldMinX, worldMaxX);
+		m_position.y = wrap(m_position.y, worldMinY, worldMaxY);
+	}
+
+	void Ship::ChangeShip()
+	{
+		/*
+			****** Little ship******
+								*/
+		/*
+			
+			m_points.push_back(Math::Vector2(0.0, 20.0);
+			m_points.push_back(Math::Vector2( 12.0,  -10.0);
+			m_points.push_back(Math::Vector2( 6.0, -4.0);
+			m_points.push_back(Math::Vector2( -6.0, -4.0);
+			m_points.push_back(Math::Vector2( -12.0, -10.0);*/
+
+		/* 
+			
+			==***Kinda the one i wanted to make***==
+			m_points.push_back(Math::Vector2(0.0, 90.0);
+			m_points.push_back(Math::Vector2( 3.0,  84.0);
+			m_points.push_back(Math::Vector2( 9.5, 84.0);
+			m_points.push_back(Math::Vector2( 12.0, 78.0);
+			m_points.push_back(Math::Vector2( 12.0, 60.0);
+			m_points.push_back(Math::Vector2(60.0, 30.0);
+			m_points.push_back(Math::Vector2( 60.0,  18.0);
+			m_points.push_back(Math::Vector2( 12.0, 18.0);
+			m_points.push_back(Math::Vector2( 7.2, 0.0);
+			m_points.push_back(Math::Vector2( 0.0, 0.0);
+			m_points.push_back(Math::Vector2(-7.2, 0.0);
+			m_points.push_back(Math::Vector2( -12.0, 18.0);
+			m_points.push_back(Math::Vector2( -60.0, 18.0);
+			m_points.push_back(Math::Vector2( -60.0, 30.0);
+			m_points.push_back(Math::Vector2( -12.0, 60.0);
+			m_points.push_back(Math::Vector2(-12.0, 78.0);
+			m_points.push_back(Math::Vector2( -9.5,  84.0);
+			m_points.push_back(Math::Vector2( -3.0, 84.0);
+			m_points.push_back(Math::Vector2( 0.0, 90.0);
+			*/
+
+		/* ===========BIG SHIP============*/
+
+	m_points.push_back(Math::Vector2(0, -12));
+		m_points.push_back(Math::Vector2(4, -6));
+		m_points.push_back(Math::Vector2(5, -5));
+		m_points.push_back(Math::Vector2(6, -5));
+		m_points.push_back(Math::Vector2(7, -6));
+		m_points.push_back(Math::Vector2(8, -8));
+		m_points.push_back(Math::Vector2(9, -6));
+		m_points.push_back(Math::Vector2(10, -5));
+		m_points.push_back(Math::Vector2(11, -5));
+		m_points.push_back(Math::Vector2(12, -6));
+		m_points.push_back(Math::Vector2(13, -8));
+		m_points.push_back(Math::Vector2(13, -9));
+		m_points.push_back(Math::Vector2(12, -11));
+		m_points.push_back(Math::Vector2(11, -12));
+		m_points.push_back(Math::Vector2(14, -11));
+		m_points.push_back(Math::Vector2(17, -9));
+		m_points.push_back(Math::Vector2(19, -7));
+		m_points.push_back(Math::Vector2(20, -5));
+		m_points.push_back(Math::Vector2(21, -2));
+		m_points.push_back(Math::Vector2(21, 1));
+		m_points.push_back(Math::Vector2(20, 4));
+		m_points.push_back(Math::Vector2(17, 8));
+		m_points.push_back(Math::Vector2(14, 10));
+		m_points.push_back(Math::Vector2(11, 11));
+		m_points.push_back(Math::Vector2(12, 10));
+		m_points.push_back(Math::Vector2(13, 8));
+		m_points.push_back(Math::Vector2(13, 7));
+		m_points.push_back(Math::Vector2(12, 5));
+		m_points.push_back(Math::Vector2(11, 4));
+		m_points.push_back(Math::Vector2(9, 3));
+		m_points.push_back(Math::Vector2(6, 3));
+		m_points.push_back(Math::Vector2(3, 4));
+		m_points.push_back(Math::Vector2(3, 12));
+		m_points.push_back(Math::Vector2(1, 9));
+		m_points.push_back(Math::Vector2(-1, 9));
+		m_points.push_back(Math::Vector2(-3, 12));
+		m_points.push_back(Math::Vector2(-3, 4));
+		m_points.push_back(Math::Vector2(-6, 3));
+		m_points.push_back(Math::Vector2(-9, 3));
+		m_points.push_back(Math::Vector2(-11, 4));
+		m_points.push_back(Math::Vector2(-12, 5));
+		m_points.push_back(Math::Vector2(-13, 8));
+		m_points.push_back(Math::Vector2(-12, 10));
+		m_points.push_back(Math::Vector2(-11, 11));
+		m_points.push_back(Math::Vector2(-14, 10));
+		m_points.push_back(Math::Vector2(-17, 8));
+        m_points.push_back(Math::Vector2(-19, 6));
+		m_points.push_back(Math::Vector2(-20, 4));
+		m_points.push_back(Math::Vector2(-21, 1));
+		m_points.push_back(Math::Vector2(-21, -2));
+		m_points.push_back(Math::Vector2(-20, -5));
+		m_points.push_back(Math::Vector2(-19, -7));
+		m_points.push_back(Math::Vector2(-17, -9));
+		m_points.push_back(Math::Vector2(-14, -11));
+		m_points.push_back(Math::Vector2(-11, -12));
+		m_points.push_back(Math::Vector2(-12, -11));
+		m_points.push_back(Math::Vector2(-13, -9));
+		m_points.push_back(Math::Vector2(-13, -8));
+		m_points.push_back(Math::Vector2(-12, -6));
+		m_points.push_back(Math::Vector2(-11, -5));
+		m_points.push_back(Math::Vector2(-9, -6));
+		m_points.push_back(Math::Vector2(-8, -8));
+        m_points.push_back(Math::Vector2(-7, -6));
+		m_points.push_back(Math::Vector2(-6, -5));
+		m_points.push_back(Math::Vector2(-5, -5));
+		m_points.push_back(Math::Vector2(-4, -6));
+		m_points.push_back(Math::Vector2(0, -12));
+	}
+	void Ship::Render()
+	{
+		glLoadIdentity();
+		glTranslatef(m_position.x, m_position.y, 0.0);
+		glRotatef(m_angle, 0.0f, 0.0f, 1.0f);
+		glBegin(GL_LINE_LOOP);
+		std::vector<Math::Vector2>::iterator it = m_points.begin();
+		for (; it != m_points.end(); ++it)
+		{
+			glVertex2f((*it).x, (*it).y);
+		}
+		glEnd();
+	};
+} // namespace Engine
--- main.cpp
+++ main.cpp
@@ -1,78 +1,32 @@
 #include <iostream>
-#include "car.hpp"
-#include "rectangle.hpp"
-#include "Vector2.hpp"
 
+//
+#include "App.hpp"
 
+const int WIDTH = 1136;
+const int HEIGHT = 640;
 
-int main(int argc, char ** argv, const char ** str2)
-{    
-    /*
-        char 8 bits (1 byte)
-        short 
-        int 32 bits (4 bytes)
-        float
-        long
-        long long
-        bool
-        void
+int main(int argc, char *argv[])
+{
+	// Create Game Object
+	//
+	Engine::App *app = new Engine::App("Asteroids!", WIDTH, HEIGHT);
 
-        |-------------------------------|
-        | (type) (name) = (val) [addr]  |
-        | int x = 4; (32 bits)          |
-        | int y = 1;                    |        
-        |-------------------------------|
-    */
+	// Initialize game
+	//
+	if (!app->Init())
+	{
+		std::cout << "App Init error!\n";
+		return -1;
+	}
 
-    int x, y;
-    x = 10;
-    y = 20;
-    
-    std::cout << "An int variable takes " << sizeof(x) << " bytes of memory" << std::endl;
-    std::cout << "Rectangle area = " << x * y << std::endl;
+	// Execute game
+	//
+	app->Execute();
 
-    // Object: Instance of a class
-    // rect is an instance of Rectangle
-    Rectangle rect(x, y);
-    Rectangle rect1(200, 100);
-    Rectangle rect2(200, 100);
-    Rectangle rect3(70, 1);
+	// Delete game object
+	//
+	delete app;
 
-    // dot operator to access public members from our Rectangle class
-    std::cout << "Rectangle area = " << rect.area() << std::endl;
-    std::cout << "Rectangle area = " << rect1.area() << std::endl;
-    std::cout << "Rectangle area = " << rect2.area() << std::endl;
-    std::cout << "Rectangle area = " << rect3.area() << std::endl;
-
-    std::cout << "A Rectangle variable takes " << sizeof(rect) << " bytes of memory" << std::endl;
-
-    // Client code that uses the Car class
-    // Object: Instance of a class
-    // bmw is an instance of Car
-    Car bmw("BMW", 180);
-    Car honda("Honda", 220);
-    Car renault("Renault", 175);
-    Car toyota("Toyota", 0);
-
-    //Object
-    bmw.drive();
-    honda.drive();
-    renault.drive();
-    toyota.drive();
-
-    //3 New Vecotrs in my Vector2 Class
-
-    Motor MotorXY(x, y);
-    Motor Motor1(100, 8);
-    Motor Motor2(100, 11);
-    Motor Motor3(100, 10.5);
-    Motor Motor4(100, 12);
-
-    //Print Instance for Vectors    
-    cout << "Velocidad maxima = " << Motor1.velocidad() << endl;
-    cout << "Velocidad Maxima = " << Motor2.velocidad() << endl;
-    cout << "Velocidad Maxima = " << Motor3.velocidad() << endl;
-    cout << "Velocidad Mexima = " << Motor4.velocidad() << endl;
-
-    return 0;
+	return 0;
 }
